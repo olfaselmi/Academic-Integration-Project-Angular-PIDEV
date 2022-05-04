df36630a9449111426684e846f28c331874912d6
olfaselmi <47832011+olfaselmi@users.noreply.github.com>
2022-05-04 16:48:48 +0100

    .

---
+++ src/app/components/users/users-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ConnectionComponent } from './connection/connection.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { MainUserComponent } from './main-user/main-user.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users.component';
+
+const routes: Routes = [
+  { path: 'uc', component: UsersComponent },
+  {path: 'inscription', component: InscriptionComponent},
+  {path : 'connexion', component: ConnectionComponent},
+  {path : 'profile', component: ProfileComponent},
+  {path : '', component: MainUserComponent},
+  {path : 'forgetPassword', component: ForgetPasswordComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
