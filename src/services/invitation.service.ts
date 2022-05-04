e5f984b3d923c2adaeabc7b5eb5d7367ec405f4d
olfaselmi <47832011+olfaselmi@users.noreply.github.com>
2022-05-04 20:11:16 +0100

    invitation+crud

--- src/services/invitation.service.ts
+++ src/services/invitation.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { invitation } from 'src/app/Model/Invitation';
 @Injectable({
   providedIn: 'root'
 })
 export class InvitationService {
+  listopportunities : string[];
+  httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+    
+    })
+  }
 
-  constructor() { }
+  constructor(private myHttp : HttpClient) { }
+  getAllOpportunities(): Observable<invitation[]> {
+    return this.myHttp.get<invitation[]>('http://localhost:8089/SpringMVC/opportunity/retrieve-all-opportunities');
+  }
+
+  deleteopportunityById(id:number){
+    return this.myHttp.delete("http://localhost:8089/SpringMVC/Invitation/remove-Invitation/{idInvitation}"+id);
+    }
+
+    addOpportunity(opportunity:invitation):Observable<invitation>{
+      return this.myHttp.post<invitation>("http://localhost:8089/SpringMVC/Invitation/addInvitation",opportunity,this.httpOptions);
+      }
+
+      getOpportunityById(idOpportunity:number):Observable<invitation>{
+        return this.myHttp.get<invitation>("http://localhost:8089/SpringMVC/opportunity/retrieve-opportunity/"+idOpportunity);
+        }
+  
+ 
+        updateOpportunity (id: number, opportunity: invitation): Observable<invitation> {
+         return this.myHttp.put<invitation>('http://localhost:8089/SpringMVC/opportunity/modify-oppotunity-byID/'+ id, opportunity,
+         this.httpOptions);
+         }
+
 }
